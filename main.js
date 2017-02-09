49fff61150cc48bf3d2480baccf45ea5f2b573ff
Tamas Balint <balint.tamas87@gmail.com>
2017-02-09 16:28:20 +0000

    add fontawesome twitter button to tweet quote & create array of objects for quotes

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
-"use strict"
+"use strict";
 
-var arrayOfQuotes = ["Patience is a virtue, and I'm learning patience. It's a tough lesson.", 
-"When something is important enough, you do it even if the odds are not in your favor.", 
-"I would like to die on Mars. Just not on impact.", 
-"Brand is just a perception, and perception will match reality over time. Sometimes it will be ahead, other times it will be behind. But brand is simply a collective impression some have about a product."];
-var lastQuote = "";
-var btnNewQuote = document.getElementById("btnNewQuote");
-var quote = document.getElementById("quote");
+(function () {
 
-btnNewQuote.addEventListener("click", showNewQuote, false);
+	var arrayOfQuotes = [{author: "Elon Musk1", quote: "1Brand is just a perception, and perception will match reality over time. Sometimes it will be ahead, other times it will be behind. But brand is simply a collective impression some have about a product."},
+		{author: "Elon Musk2", quote: "2Patience is a virtue, and I'm learning patience. It's a tough lesson."},
+		{author: "Elon Musk3", quote: "3When something is important enough, you do it even if the odds are not in your favor."},
+		{author: "Elon Musk4", quote: "4I would like to die on Mars. Just not on impact."},];
 
-function showQuote (theRandomQuote) {
-	quote.innerHTML = theRandomQuote;
-	quote.style.visibility = "visible";
-}
+	var lastQuote = "";
+	var btnNewQuote = document.getElementById("btnNewQuote");
+	var quote = document.getElementById("quote");
+	var author = document.getElementById("author");
+	var btnTwitterShare = document.getElementById("twitter-share-button");
 
-function showNewQuote () {
-	// if randomQuote is not defined here than I get undefined returned
-	var randomQuote = arrayOfQuotes[Math.floor(Math.random() * arrayOfQuotes.length)];
-	if (lastQuote === randomQuote) {
-		showNewQuote();
+	btnNewQuote.addEventListener("click", showNewQuote, false);
+	btnNewQuote.addEventListener("click", updateQuoteToTweet, false);
+	btnNewQuote.addEventListener("click", showAuthor, false);
+
+	function showQuote (theRandomQuote) {
+		quote.innerHTML = theRandomQuote;
+		quote.style.visibility = "visible";
 	}
-	else if (lastQuote !== randomQuote) {
-		console.log( randomQuote );
-		showQuote(randomQuote);
-		lastQuote = randomQuote;
+
+	function showAuthor (theRandomArrayIndex) {
+		author.innerHTML = arrayOfQuotes[theRandomArrayIndex].author;
 	}
-}
+
+		function updateQuoteToTweet () {
+		btnTwitterShare.setAttribute("href", "https://twitter.com/intent/tweet?text=" + encodeURI(lastQuote));
+	}
+
+	function showNewQuote () {
+		// if randomQuote is not defined here than I get undefined returned
+		var randomArrayIndex = Math.floor(Math.random() * arrayOfQuotes.length);
+		var randomQuote = arrayOfQuotes[randomArrayIndex].quote;
+
+		if (lastQuote === randomQuote) {
+			showNewQuote();
+		}
+		else if (lastQuote !== randomQuote) {
+			console.log( randomQuote );
+			showQuote(randomQuote);
+			showAuthor(randomArrayIndex);
+			lastQuote = randomQuote;
+		}
+	}
+})();
